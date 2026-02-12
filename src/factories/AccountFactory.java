37e83b32d5873af4ecebad951546031cc21b53a5
DavidB <david.bernard@fms-ea.com>
2026-02-12 14:35:36 +0100

    accountfactory

---
+++ src/factories/AccountFactory.java
@@ -0,0 +1,42 @@
+package factories;
+
+import models.Account;
+import models.CurrentAccount;
+import models.SavingsAccount;
+
+public class AccountFactory {
+
+  public static Account createAccount(
+    String accountType,
+    String ownerFirstName,
+    String ownerLastName,
+    String creationDate,
+    Number balance,
+    String accountNumber,
+    double interestRate
+  ) {
+    if (accountType.equalsIgnoreCase("savings")) {
+      return new SavingsAccount(
+        ownerFirstName,
+        ownerLastName,
+        creationDate,
+        balance,
+        accountNumber,
+        interestRate
+      );
+    } else if (accountType.equalsIgnoreCase("current")) {
+      return new CurrentAccount(
+        ownerFirstName,
+        ownerLastName,
+        creationDate,
+        balance,
+        accountNumber,
+        interestRate
+      );
+    } else {
+      throw new IllegalArgumentException(
+        "Invalid account type: " + accountType
+      );
+    }
+  }
+}
